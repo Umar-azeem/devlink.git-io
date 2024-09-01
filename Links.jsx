39e2eee4a7afefb40c47a0c4e58668faaf94b0b5
umar azeem <125313441+Umar-azeem@users.noreply.github.com>
2024-09-01 02:22:36 -1200

    Update Links.jsx

--- Links.jsx
+++ Links.jsx
@@ -9,7 +9,7 @@ function Links({ links, setLinks }) {
       setLinks([...links, links.length + 1]);
     }
   };
-  console.log("lliinnkkss",links)
+  console.log("lliinnkkss", links);
   const deleteListItem = (key) => {
     // = [0,1,2,3] - key = 3
     const filteredArray = links.filter((e, i) => i !== key);
@@ -18,37 +18,44 @@ function Links({ links, setLinks }) {
 
   return (
     <>
-      <div className="w-2/4 h-auto bg-white m-4">
+      <div className=" h-auto bg-white sm:m-3">
         <div className="flex flex-col justify-center">
-          <h1 className="text-4xl m-8 font-bold">Customize your links</h1>
-          <p className="text-gray-500 ml-8 text-lg">
+          <h1 className="sm:text-4xl text-md sm:m-8 m-4 font-bold">
+            Customize your links
+          </h1>
+          <p className="text-gray-500 sm:ml-8 ml-4 mr-4 sm:mr-8 text-[10px] sm:text-lg">
             Add/edit/remove links below and then share all your profiles with
             the world
           </p>
           <button
             onClick={handleAddLink}
-            className="hover:bg-violet-100 text-indigo-500 text-lg mt-12 font-medium rounded-md ml-4 mr-4 h-12 border-[1px] border-indigo-500"
+            className="hover:bg-violet-100 text-indigo-500 sm:text-lg text-[10px] sm:mt-12 mt-4 font-medium rounded-md ml-2 sm:ml-4 mr-2 sm:mr-4 sm:h-12 h-7 border-[1px] border-indigo-500"
           >
             + Add new link
           </button>
         </div>
-        <div className="w-[90%] h-auto rounded-lg bg-gray-50 m-12 flex flex-col justify-center">
+        <div className="h-auto rounded-lg bg-gray-50 flex flex-col justify-center">
           {Array.isArray(links) && links.length > 0 ? (
             links.map((link, index) => (
               <div key={index} className="m-4">
-                <Add key={index} linkNumber={link} index={index} deleteItem={deleteListItem} />
+                <Add
+                  key={index}
+                  linkNumber={link}
+                  index={index}
+                  deleteItem={deleteListItem}
+                />
               </div>
             ))
           ) : (
-            <div className="w-[90%] h-auto rounded-lg bg-gray-50 m-12 flex flex-col justify-center">
+            <div className=" h-auto rounded-lg bg-gray-50 flex flex-col justify-center">
               <div className="w-full h-auto flex justify-center">
-                <img src={pic1} className="w-72 h-72" alt="Start" />
+                <img src={pic1} className="sm:w-72 sm:h-72 w-40 h-40" alt="Start" />
               </div>
               <div>
-                <h1 className="text-4xl text-center font-bold text-gray-900">
+                <h1 className="sm:text-4xl text-2xl text-center font-bold text-gray-900">
                   Let's get you started
                 </h1>
-                <p className="text-center text-sm m-8">
+                <p className="text-center text-[10px] sm:text-sm m-2 sm:m-8">
                   Use the “Add new link” button to get started. Once you have
                   more <br />
                   than one link, you can reorder and edit them. We’re here to
@@ -61,8 +68,8 @@ function Links({ links, setLinks }) {
         </div>
         <div>
           <hr />
-          <div className="flex justify-end mt-8 mr-6">
-            <button className="text-white bg-violet-700 text-lg font-medium rounded-md w-28 h-12 border-[1px] border-indigo-500 hover:border-indigo-100 hover:bg-violet-300">
+          <div className="flex justify-end mt-2 sm:mt-8 mr-6">
+            <button className="text-white bg-violet-700   text-sm  sm:text-lg font-medium rounded-md w-14 h-8 sm:w-28 sm:h-12 border-[1px] border-indigo-500 hover:border-indigo-100 hover:bg-violet-300">
               Save
             </button>
           </div>
